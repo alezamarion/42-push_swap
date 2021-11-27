85c65254e79ba89ebbe27c80773cdf2ae0737f29
Alexandre Zamarion Cepeda <alexandre@Alexandres-MacBook-Air.local>
2021-11-27 10:59:25 -0300

    .h

---
+++ includes/push_swap.h
@@ -0,0 +1,26 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+#include "../libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct  s_node
+{
+    int             data;
+    struct s_node   *next;
+}t_node;
+
+typedef struct s_stack
+{
+    t_node  *head;
+    int     size;
+}t_stack;
+
+typedef struct s_stacks
+{
+    t_stack *stack_a;
+    t_stack *stack_b;
+}t_stacks;
+
+#endif
